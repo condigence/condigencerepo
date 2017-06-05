3a1835c554503d8c2e5155fa435394659dd560f3
Vishal <condigence@gmail.com>
2017-06-05 23:08:56 +0530

    Removed v0.1 Medicare Project

--- Medicare/src/main/java/com/condigence/medicare/repository/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.condigence.medicare.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.condigence.medicare.model.Appointment;
-
-public interface AppointmentRepository extends CrudRepository<Appointment, Integer> {
-
-}
--- Medicare/src/main/java/com/condigence/medicare/util/SearchCriteria.java
+++
@@ -1,7 +0,0 @@
-//package com.condigence.medicare.util;
-//
-//public class SearchCriteria {
-//	private String key;
-//	private String operation;
-//	private Object value;
-//}

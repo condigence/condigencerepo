5bb80eade5deb6b9e3d61356fb664b96b0a2497d
Vishal <condigence@gmail.com>
2017-06-26 00:24:22 +0530

    v0.7 commit by Vishal for Medicare Project 26 6 2017

---
+++ Medicare/src/main/java/com/condigence/medicare/controllers/HomeController.java
@@ -0,0 +1,84 @@
+package com.condigence.medicare.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	// inject via application.properties
+	@Value("${welcome.message:test}")
+	private String message = "Hello World";
+
+	/*@RequestMapping("/")
+	public String login() {
+		return "index";
+	}*/
+	
+	@RequestMapping("/chart")
+	public String chart() {
+		return "chart";
+	}
+
+	@RequestMapping("/home")
+	public String home() {
+		return "home";
+	}
+
+	@RequestMapping("/dashboard")
+	public String dashboard() {
+		return "dashboard";
+	}
+
+	@RequestMapping("/user")
+	public String user() {
+		return "user";
+	}
+
+	@RequestMapping("/appointment")
+	public String appointment() {
+		return "appointment";
+	}
+
+	@RequestMapping("/doctor")
+	public String doctor() {
+		return "doctor";
+	}
+
+	@RequestMapping("/patient")
+	public String patient() {
+		return "patient";
+	}
+
+	@RequestMapping("/user-type")
+	public String userType() {
+		return "user-type";
+	}
+
+	@RequestMapping("/service-type")
+	public String serviceType() {
+		return "service";
+	}
+
+	@RequestMapping("/commission")
+	public String commission() {
+		return "commission";
+	}
+
+	@RequestMapping("/billing")
+	public String billing() {
+		return "billing";
+	}
+
+	@RequestMapping("/report")
+	public String report() {
+		return "report";
+	}
+	
+	@RequestMapping("/print")
+	public String print() {
+		return "print";
+	}
+
+}
---
+++ Medicare/src/main/resources/static/db/medicare.dml.sql
@@ -0,0 +1,13 @@
+use medicare_db;
+
+INSERT INTO `role` VALUES (1,'SUPERADMIN');
+INSERT INTO `role` VALUES (2,'ADMIN');
+
+
+INSERT INTO `medicare_db`.`doctor` (`name`, `contact_no`, `email`, `hospital_name`, `qualification`, `speciality`, `address`, `commission`, `added_by_user_id`) VALUES ('Vishal', '9742503868', 'test@gmail.com', 'HSR', 'MBBS', 'Brain', 'HSR', '50000', '1');
+
+INSERT INTO `medicare_db`.`patient` (`first_name`, `last_name`, `age`, `address`, `email`, `contact_number`, `gender`) VALUES ('Chita', 'Ranjan', '30', 'SMNagar', 'abc@gnamil', '7476476', 'M');
+
+INSERT INTO `medicare_db`.`service_type` (`service_type`, `price`, `created_by_user_id`) VALUES ('ECG', '5000', '1');
+
+INSERT INTO `medicare_db`.`appointment` (`patient_id`, `referred_by_doctor_id`, `service_type_id`, `created_by_user_id`, `slot`) VALUES ('1', '1', '1', '1', '1');

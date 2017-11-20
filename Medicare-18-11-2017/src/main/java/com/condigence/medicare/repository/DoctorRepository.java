08aebbbc72940406cbebe7244b783b085b5ef324
Vishal <condigence@gmail.com>
2017-11-21 00:42:27 +0530

    21-11-2017 commit

--- Medicare-18-11-2017/src/main/java/com/condigence/medicare/repository/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.condigence.medicare.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.condigence.medicare.model.Doctor;
-
-public interface DoctorRepository extends CrudRepository<Doctor, Integer> {
-
-}
--- Medicare/src/main/resources/static/js/validation.js
+++ Medicare/src/main/resources/static/js/validation.js
@@ -241,55 +241,122 @@
   
  /*---------------------form Validation to Add Service----------------------*/ 
  
- /*---------------------form Validation to Add service----------------------*/
+ /*---------------------form Validation to Add Patient----------------------*/
+ $('#addPatientform').formValidation({
+     framework: 'bootstrap',
+     excluded: ':disabled',
+     icon: {
+         valid: 'glyphicon glyphicon-ok',
+         invalid: 'glyphicon glyphicon-remove',
+         validating: 'glyphicon glyphicon-refresh'
+     },
+     fields: {
+    	 add_patient_firstname: {
+             validators: {
+                 notEmpty: {
+                     message: 'The doctor name is required'
+                 },
+                 regexp: {
+                     regexp: /^[a-zA-Z0-9_\.]+$/,
+                     message: 'The username can only consist of alphabetical, number, dot and underscore'
+                 }
+             }
+         },
+         add_patient_lastName: {
+             validators: {
+                 notEmpty: {
+                     message: 'The doctor name is required'
+                 },
+                 regexp: {
+                     regexp: /^[a-zA-Z0-9_\.]+$/,
+                     message: 'The username can only consist of alphabetical, number, dot and underscore'
+                 }
+             }
+         },
+         add_patient_contactNo: {
+             validators: {
+                 notEmpty: {
+                     message: 'The contact No. is required'
+                 },stringLength: {
+                     min: 10,
+                     max: 15,
+                     message: 'The username must be more than 10 and less than 15 characters long'
+                 },
+                 regexp: {
+                     message: 'The phone number can only contain the digits, spaces, -, (, ), + and .',
+                     regexp: /^[0-9\s\-()+\.]+$/
+                 }
+             }
+         },
+         add_patient_email: {
+             validators: {
+                 notEmpty: {
+                     message: 'The email is required'
+                 },
+                 emailAddress: {
+                     message: 'The input is not a valid email address'
+                 }
+             }
+         },
+         add_patient_gender: {
+             validators: {
+                 notEmpty: {
+                     message: 'The gender is required'
+                 }
+             }
+         },
+         add_patient_age: {
+             validators: {
+                 notEmpty: {
+                     message: 'The patient age is required'
+                 },
+                 regexp: {
+                     message: 'The commission can only contain the digits, spaces, -, (, ), + and .',
+                     regexp: /^[0-9\s\-()+\.]+$/
+                 }
+             }
+         },
+        
+         add_patient_address: {
+             validators: {
+                 notEmpty: {
+                     message: 'The doctor address is required'
+                 }
+             }
+         }
+         }
+ }).on('success.form.fv', function(e) {
+ 	
+ 	e.preventDefault();
+ 	
+ 	var firstName = $(".add_patient_firstname").val();				
+ 	var lastName = $(".add_patient_lastName").val();
+	var contactNo =	$(".add_patient_contactNo").val();
+	var email =	$(".add_patient_email").val();
+	var gender = $(".add_patient_gender").val();
+	var age = $(".add_patient_age").val();
+	var address =	$(".add_patient_address").val();
 
- $('#addServiceform').formValidation({
-        framework: 'bootstrap',
-        excluded: ':disabled',
-        icon: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        fields: {
-        	add_new_serviceType: {
-                validators: {
-                    notEmpty: {
-                        message: 'The Service name is required'
-                    }
-                }
-            },
-            add_new_serviceType_price: {
-                validators: {
-                    notEmpty: {
-                        message: 'The Service price is required'
-                    },
-                    regexp: {
-                        message: 'The commission can only contain the digits, spaces, -, (, ), + and .',
-                        regexp: /^[0-9\s\-()+\.]+$/
-                    }
-                }
-            }
-        }
-    }).on('success.form.fv', function(e) {
-    	
-    	e.preventDefault();
-    	
-    	var name = $("#add_new_serviceType").val();
-		var price = $("#add_new_serviceType_price").val();
+	var jsondata = {};
+	jsondata["firstName"] = firstName;
+	jsondata["lastName"] = lastName;
+	jsondata["contactNo"] = contactNo;
+	jsondata["email"] = email;
+	jsondata["gender"] = gender;
+	jsondata["age"] = age;
+	jsondata["address"] = address;			
+	jsondata["addedByUserId"] = 1;
 
-		var jsondata = {};
-		jsondata["name"] = name;
-		jsondata["price"] = price;
+	
+	var doctorAddUrl = baseUrl + "/patient/add";
+	alert(JSON.stringify(jsondata));
+	callAjaxPostJSON(doctorAddUrl, "POST", "", jsondata);
 
-		var serviceAddUrl = baseUrl + "/servicetype/add";
-
-		callAjaxPostJSON(serviceAddUrl, "POST", "", jsondata);
-		 $("#addUser").modal('hide');
+		 $("#addPatient").modal('hide');
 		 location.reload(true);
-    });
-  
- /*---------------------form Validation to Add Service----------------------*/ 
+ });
+
+/*---------------------form Validation to Add doctor----------------------*/
  
     
     $('.modal').on('shown.bs.modal', function() {

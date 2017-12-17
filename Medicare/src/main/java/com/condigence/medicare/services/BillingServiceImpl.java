b0836b8a594837cf58e70de1f6674ff228c29418
Vishal <condigence@gmail.com>
2017-12-18 01:53:11 +0530

    18-12-2017-01-53 commit

---
+++ Medicare/src/main/java/com/condigence/medicare/services/BillingServiceImpl.java
@@ -0,0 +1,46 @@
+package com.condigence.medicare.services;
+
+import java.util.Date;
+import java.util.Random;
+
+import org.springframework.stereotype.Service;
+
+import com.condigence.medicare.dto.Billing;
+import com.condigence.medicare.model.Appointment;
+import com.condigence.medicare.model.ServiceType;
+
+@Service("billingService")
+public class BillingServiceImpl  implements BillingService {
+
+	
+	
+	@Override
+	public Billing getBill(Appointment appointment) {
+		
+		double totalPrice = 0;
+		
+		Billing  billing = new Billing();
+		
+		billing.setName(appointment.getPatient().getFirstName());
+		billing.setListofServices(appointment.getServices());
+		billing.setDate(new Date());
+		billing.setBillNo(new Random().nextInt(10000));
+		
+		for(ServiceType s: appointment.getServices()) {
+			totalPrice += s.getPrice(); 
+//			System.out.println(" service "+s.getName());
+//			System.out.println(" service price "+s.getPrice());
+			
+		}
+		
+		billing.setTotalPrice(totalPrice);
+		
+//		System.out.println(" hello ...... "+ billing.getBillNo());
+//		System.out.println(" hello ...... "+ billing.getName());
+//		System.out.println(" hello ...... "+ billing.getTotalPrice());
+//		System.out.println(" hello ...... "+ billing.getTotalPrice());
+		
+		return billing;
+	}
+
+}

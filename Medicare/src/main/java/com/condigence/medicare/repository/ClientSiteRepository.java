fda3a7f75986cf51570d7b41ed840c1a3ca5a47e
condigence <condigence@gmail.com>
2022-09-29 19:09:04 +0530

    removed other files

--- Medicare/src/main/java/com/condigence/medicare/repository/ClientSiteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.condigence.medicare.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.condigence.medicare.model.ClientSite;
-
-public interface ClientSiteRepository extends CrudRepository<ClientSite, Long> {
-
-}
